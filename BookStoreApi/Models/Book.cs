20cc7a47293040f9842f8c2bc2c55fb837e6cb88
Yhors Brayan <rs.brayan26@gmail.com>
2024-04-14 08:45:48 -0500

    Agregar archivos de proyecto.

---
+++ BookStoreApi/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("name")]
+        public string BookName { get; set; } = null!;
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+        [BsonElement("category")]
+        public string Category { get; set; } = null!;
+        [BsonElement("autor")]
+        public string Author { get; set; } = null!;
+
+    }
+}
